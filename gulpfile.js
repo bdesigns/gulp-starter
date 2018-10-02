3ed92aef2edaf9e7edc2e4a7c8a57f976bb186e7
Brad E <brade@Mac-Pro.local>
2018-10-02 15:44:24 -0400

    Clean up

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,6 @@ const browserSync  = require('browser-sync').create();
 const sass         = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 
-/* 
-    -- top level functions --
-    gulp.task - define task
-    gulp.src - point to file to use
-    gulp.dest - points to folder to output
-    gulp.watch - watch files and folders for change
-*/
-
 // Compile Sass & Inject Into Browser
 gulp.task('sass', function() {
     return gulp.src(['src/scss/*.scss'])
@@ -23,7 +15,6 @@ gulp.task('sass', function() {
         .pipe(gulp.dest("src/css"))
         .pipe(browserSync.stream());
 });
-
 
 // Watch Sass & Serve
 gulp.task('serve', ['sass'], function() {
