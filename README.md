3a8363dd57898fbc62aa4b26b705f13992781650
Brad <badz@bdesigns.net>
2024-12-02 18:00:45 -0500

    Update kit with new js commands

--- README.md
+++ README.md
@@ -10,12 +10,19 @@ Download or Clone this repo into a local folder
 $ npm install
 ```
 
-Install the dependencies (gulp, gulp-sass, browser-sync, gulp-autoprefixer)
+Install the dependencies{
+                            
+        (gulp)              npm install --global gulp-cli 
+        (gulp-sass)         npm install sass gulp-sass --save-dev 
+        (gulp-browser-sync) npm install -g browser-sync 
+        (gulp-autoprefixer) npm install --save-dev gulp-autoprefixer
+
+}
 
 ## Run
 
 This will watch your project files, compile them and run a dev server in localhost.
 
 ```
-$ npm start
+$ gulp start
 ```
--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,42 @@
-const gulp         = require('gulp');
-const browserSync  = require('browser-sync').create();
-const sass         = require('gulp-sass');
-const autoprefixer = require('gulp-autoprefixer');
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass')(require('sass'));  // Compile Sass
+const autoprefixer = () => import('gulp-autoprefixer'); // Dynamically import autoprefixer
 
-// Compile Sass & Inject Into Browser
-gulp.task('sass', function() {
-    return gulp.src(['src/scss/*.scss'])
-        .pipe(sass())
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
+// Task to compile Sass files
+gulp.task('sass', async function() {
+    const autoprefixerModule = await autoprefixer(); // Wait for autoprefixer to be loaded
+
+    return gulp.src('src/scss/**/*.scss')
+        .pipe(sass({ style: 'compressed' }).on('error', sass.logError))  // Compile SCSS to CSS expanded | compressed
+        .pipe(autoprefixerModule.default({  // Add vendor prefixes
+            overrideBrowserslist: ['last 1 versions', 'IE 11'],
             cascade: false
         }))
-        .pipe(sass.sync().on('error', sass.logError))
-        .pipe(gulp.dest("src/css"))
-        .pipe(browserSync.stream());
+        .pipe(gulp.dest('src/css'))  // Output compiled CSS to the 'dist/css' folder
+        .pipe(browserSync.stream());  // Inject CSS changes into the browser without a full reload
 });
 
-// Watch Sass & Serve
-gulp.task('serve', ['sass'], function() {
+// Task to serve files with BrowserSync and auto-reload
+gulp.task('serve', function() {
+    // Start a local server
     browserSync.init({
-        server: "./src"  
+        server: {
+            baseDir: 'src'  // Set the root folder for serving files
+        },
+        notify: false,  // Disable notifications in the browser
+        open: false  // Automatically open the browser when starting the server
     });
-    gulp.watch(['src/scss/*.scss'], ['sass']);
-    gulp.watch("src/*.html").on('change', browserSync.reload);
+
+    // Watch for changes in SCSS files and recompile
+    gulp.watch('src/scss/**/*.scss', gulp.series('sass'));  // Watch SCSS files
+
+    // Watch for changes in HTML, CSS, and JS files and reload the browser
+    gulp.watch('src/*.html').on('change', browserSync.reload);  // Watch HTML files
+    gulp.watch('src/css/*.css').on('change', browserSync.reload);  // Watch CSS files
+    gulp.watch('src/js/*.js').on('change', browserSync.reload);  // Watch JS files
 });
 
-// Default Task
-gulp.task('default', ['serve']);
+
+// Default task to run when no task is specified
+gulp.task('default', gulp.series('sass', 'serve'));
